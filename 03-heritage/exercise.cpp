1476da783b2bb739622665d9fc09b408cb4239f8
Antonio Nieblas <nibj961116@gmail.com>
2020-11-25 22:52:59 -0700

    update heritage

--- 03-heritage/exercise.cpp
+++ 03-heritage/exercise.cpp
@@ -23,6 +23,23 @@ class Employee : public Person{
         void showEmployee();
 };
 
+class Student : public Person{
+    private:
+        int idStn;
+        float note;
+    public:
+        Student(string, string, string, int, int, float);
+        void showStudent();
+};
+
+class Univercitary : public Student{
+    private:
+        string degree;
+        string speciality;
+    public:
+        Univercitary(string, string, string, int, int, float, string, string);
+        void showUnivercitary();
+};
 
 Person::Person(string _name, string _lastName, string _gender, int _age){
     name = _name;
@@ -36,6 +53,18 @@ Employee::Employee(string _name, string _lastName, string _gender, int _age, str
         job = _job;
         idJob = _idJob;
         salary = _salary;
+}
+
+Student::Student(string _name, string _lastName, string _gender, int _age, int _idStn, float _note)
+        :Person(_name, _lastName, _gender, _age){
+            idStn = _idStn;
+            note = _note;
+}
+
+Univercitary::Univercitary(string _name, string _lastName, string _gender, int _age, int _idStn, float _note, string _degree, string _speciality)
+             : Student( _name, _lastName, _gender, _age, _idStn, _note){
+           degree = _degree;
+           speciality = _speciality;       
 }
 
 void Person::showPerson(){
@@ -52,14 +81,32 @@ void Employee::showEmployee(){
     cout << "Salary: " << salary << endl;
 }
 
+void Student::showStudent(){
+    showPerson();
+    cout << "ID student: " << idStn << endl;
+    cout << "Note: " << note << endl;
+}
+
+void Univercitary::showUnivercitary(){
+    showStudent();
+    cout << "Degree: " << degree << endl;
+    cout << "Speciality: " << speciality << endl;
+
+}
+
 int main(){
     Person p1("Antonio", "Nieblas", "male", 24);
     Employee e1("Antonio", "Nieblas", "male", 24, "developer", 1001, 12000);
-    
+    Student s1("Antonio", "Nieblas", "male", 24, 15130452,8.4);   
+    Univercitary u1("Antonio", "Nieblas", "male", 24, 15130452,8.4,"electronic","mechatronic");
+
     p1.showPerson();
     cout << "\n";
     e1.showEmployee();
     cout << "\n";
-
+    s1.showStudent();
+    cout << "\n";
+    u1.showUnivercitary();
+    cout << "\n";
     return 0;
 }
